5198cc39d5f76f7e6a069f86c0d98a2995177f64
Shruthi-Sivagnanam <shruthi.11012003@gmail.com>
2023-08-07 19:28:27 +0530

    VignereEncrypt

---
+++ Vignere_Cipher/VignereCipherEncrpyt.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+class VignereCipherEncrpyt {
+
+    static char[][] alphabetMatrix = new char[26][26];
+    static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    static void matrixFormation() {
+
+        for (int i = 0; i < 26; i++) {
+            int index = i;
+            for (int j = 0; j < 26; j++) {
+                if (index == 26)
+                    index = 0;
+                alphabetMatrix[i][j] = alphabet.charAt(index);
+                index++;
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+                System.out.print(alphabetMatrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String plainText, key;
+
+        matrixFormation();
+
+        System.out.println("Enter the Plain Text: ");
+        plainText = reader.next();
+        System.out.println("Enter the Key Text: ");
+        key = reader.next();
+
+        int j = 0;
+        for (int i = 0; i < plainText.length(); i++) {
+            if (j == key.length())
+                j = 0;
+            System.out.print(alphabetMatrix[alphabet.indexOf(key.charAt(j))][alphabet.indexOf(plainText.charAt(i))]);
+            j++;
+
+        }
+
+        reader.close();
+    }
+}
